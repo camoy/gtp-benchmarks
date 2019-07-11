53fc1e96667b423fabb540798df30d9683faa09b
Cameron Moy <camoy@cs.umd.edu>
2019-07-10 22:20:53 -0400

    Add tetris changes

--- benchmarks/tetris/untyped/aux.rkt
+++ benchmarks/tetris/untyped/aux.rkt
@@ -1,25 +1,22 @@
 #lang racket
 
 (require "data.rkt"
-         "tetras.rkt")  
+         "tetras.rkt")
 
 (provide
  list-pick-random
  neg-1
  tetras)
 
-(define r (make-pseudo-random-generator))
-(parameterize ((current-pseudo-random-generator r))
-  (random-seed 43453))
-
+(random-seed 43453)
 
 (define (list-pick-random ls)
-  (list-ref ls (random (length ls) r)))
+  (list-ref ls (random (length ls))))
 
 (define neg-1 -1)
 
 (define tetras
-  (list 
+  (list
    (build-tetra-blocks 'green   1/2 -3/2    0 -1 0 -2 1 -1 1 -2)
    (build-tetra-blocks 'blue    1   -1      0 -1 1 -1 2 -1 3 -1)
    (build-tetra-blocks 'purple  1   -1      0 -1 1 -1 2 -1 2 -2)
@@ -27,4 +24,3 @@
    (build-tetra-blocks 'orange  1   -1      0 -1 1 -1 2 -1 1 -2)
    (build-tetra-blocks 'red     1   -1      0 -1 1 -1 1 -2 2 -2)
    (build-tetra-blocks 'pink    1   -1      0 -2 1 -2 1 -1 2 -1)))
-
